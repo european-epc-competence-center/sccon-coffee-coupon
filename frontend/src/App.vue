a949668d5e5c036891ef7d352916ff6fe5cba4ac
F-Node-Karlsruhe <christian.fries@eecc.de>
2023-10-27 10:00:02 +0200

    add navbar
    Signed-off-by: F-Node-Karlsruhe <christian.fries@eecc.de>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,15 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <nav class="navbar navbar-dark bg-dark shadow">
+    <div class="container-lg">
+      <a class="navbar-brand" href="#">
+        <img src="@/assets/logo.png" alt="" width="80" height="80"
+          class="d-inline-block align-text-center p-1 me-3 border bg-light rounded">
+        <strong class="text-primary">SCCon Coffee Coupons</strong>
+      </a>
+    </div>
+  </nav>
   <RouterView />
   <div class="eecclogo">
     <img id="eecclogo" src="@/assets/eecc.png" />
